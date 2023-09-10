dc737e5982836047a300c7e5a780aee806a604cd
VamsiVajjhula <117925570+VamsiVajjhula-2022@users.noreply.github.com>
2023-09-10 18:02:20 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,7 @@
+print("Progaram to print the given number is even or odd")
+a=int(input("Enter a number:"))
+if (a%2==0):
+    print("The number is even number")
+else:
+    print("The numbe is odd number")
+    
