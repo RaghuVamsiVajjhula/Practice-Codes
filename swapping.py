b36af6c76e5bb911db05ed2df0297ba37d19c3d9
VamsiVajjhula <117925570+VamsiVajjhula-2022@users.noreply.github.com>
2023-09-12 18:43:55 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("The value of a before swapping:",a)
+print("The value of b before swapping:",b)
+temp=a
+a=b
+b=temp
+print("The value of a after swapping:",a)
+print("The value of b after swapping:",b)
